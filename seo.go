d7874cd7b005b7271d9ae14e5c95ae310b89576d
binku <binku87@gmail.com>
2015-10-28 16:00:20 +0800

    Get tags from struct

--- seo.go
+++ seo.go
@@ -2,6 +2,7 @@ package seo
 
 import (
 	"database/sql/driver"
+	"github.com/qor/qor"
 	"github.com/qor/qor/admin"
 	"os"
 	"path"
@@ -10,22 +11,23 @@ import (
 )
 
 type Setting struct {
-	Data string
+	Title       string
+	Description string
 }
 
 func (setting *Setting) Scan(value interface{}) error {
 	if bytes, ok := value.([]byte); ok {
-		setting.Data = string(bytes)
+		setting.Title = string(bytes)
 	} else if str, ok := value.(string); ok {
-		setting.Data = str
+		setting.Title = str
 	} else if strs, ok := value.([]string); ok {
-		setting.Data = strs[0]
+		setting.Title = strs[0]
 	}
 	return nil
 }
 
 func (setting Setting) Value() (driver.Value, error) {
-	return setting.Data, nil
+	return setting.Title, nil
 }
 
 var injected bool
@@ -46,8 +48,18 @@ func (Setting) ConfigureQorResource(res *admin.Resource) {
 		if field.Struct.Type == reflect.TypeOf(Setting{}) {
 			name := field.Name
 
-			res.Meta(&admin.Meta{Name: name, Type: "seo"})
+			meta := res.GetMeta(name)
+			if meta != nil {
+				meta.Type = "seo"
+			} else {
+				res.Meta(&admin.Meta{Name: name, Type: "seo"})
+				meta = res.GetMeta(name)
+			}
+
+			tags := field.Tag.Get("seo")
+			meta.Valuer = func(value interface{}, ctx *qor.Context) interface{} {
+				return tags
+			}
 		}
 	}
-
 }
