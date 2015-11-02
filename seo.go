d6747db63c97ebfb89a34b9a4431b7d347ee7b86
binku <binku87@gmail.com>
2015-11-02 15:12:34 +0800

    Render: real value from tags

--- seo.go
+++ seo.go
@@ -17,7 +17,8 @@ import (
 type Setting struct {
 	Title       string
 	Description string
-	Vars        []string
+	Tags        string
+	TagsArray   []string `json:"-"`
 }
 
 type settingInterface interface {
@@ -70,30 +71,36 @@ func (Setting) ConfigureQorResource(res *admin.Resource) {
 				meta = res.GetMeta(name)
 			}
 
-			var vars []string
-			tags := strings.Split(field.Tag.Get("seo"), ",")
-			for _, tag := range tags {
-				vars = append(vars, strings.Trim(tag, " "))
-			}
-			meta.Valuer = func(value interface{}, ctx *qor.Context) interface{} {
+			tags := field.Tag.Get("seo")
+			tagsArray := splitTags(tags)
+			meta.SetValuer(func(value interface{}, ctx *qor.Context) interface{} {
 				settingField, _ := ctx.GetDB().NewScope(value).FieldByName(name)
 				setting := settingField.Field.Interface().(settingInterface).GetSetting()
-				setting.Vars = vars
+				setting.Tags = tags
+				setting.TagsArray = tagsArray
 				return setting
-			}
+			})
 		}
 	}
 }
 
 func (setting Setting) Render(mainObj interface{}, obj interface{}) template.HTML {
-	title := replaceVars(setting.Title, mainObj, obj)
-	description := replaceVars(setting.Description, mainObj, obj)
+	title := replaceTags(setting.Title, splitTags(setting.Tags), mainObj, obj)
+	description := replaceTags(setting.Description, splitTags(setting.Tags), mainObj, obj)
 	return template.HTML(fmt.Sprintf("<title>%s</title>\n<meta name=\"description\" content=\"%s\">", title, description))
 }
 
-func replaceVars(originalVal string, mainObj interface{}, obj interface{}) string {
+// Helpers
+func replaceTags(originalVal string, validTags []string, mainObj interface{}, obj interface{}) string {
 	re := regexp.MustCompile("{{([a-zA-Z0-9]*)}}")
 	matches := re.FindAllStringSubmatch(originalVal, -1)
+	for _, match := range matches {
+		field := reflect.ValueOf(obj).FieldByName(match[1])
+		if field.IsValid() && isTagContains(validTags, match[1]) {
+			value := field.Interface().(string)
+			originalVal = strings.Replace(originalVal, match[0], value, 1)
+		}
+	}
 	for _, match := range matches {
 		field := reflect.ValueOf(mainObj).FieldByName(match[1])
 		if field.IsValid() {
@@ -102,4 +109,21 @@ func replaceVars(originalVal string, mainObj interface{}, obj interface{}) strin
 		}
 	}
 	return originalVal
+}
+
+func isTagContains(tags []string, item string) bool {
+	for _, t := range tags {
+		if item == t {
+			return true
+		}
+	}
+	return false
+}
+
+func splitTags(tags string) []string {
+	var tagsArray []string
+	for _, tag := range strings.Split(tags, ",") {
+		tagsArray = append(tagsArray, strings.Trim(tag, " "))
+	}
+	return tagsArray
 }
--- views/themes/seo/assets/javascripts/seo.js
+++ views/themes/seo/assets/javascripts/seo.js
@@ -2,9 +2,10 @@ $(".qor-seo-submit").click(function(){
   var $wrap = $(this).parents(".qor-seo");
   var fieldName = $wrap.find(".qor-seo-field").attr("name");
   var titleValue = $wrap.find(".qor-seo-title-field").val();
+  var tagsValue = $wrap.find(".qor-seo-tags-field").val();
   var descriptionValue = $wrap.find(".qor-seo-description-field").val();
   var data = {};
-  data[fieldName] = "{ \"Title\" : \"" + titleValue + "\", \"Description\" : \"" + descriptionValue + "\"}";
+  data[fieldName] = '{ "Title" : "' + titleValue + '", "Description" : "' + descriptionValue + '", "Tags" : "' + tagsValue + '"}';
   console.info(data);
   $.ajax({
     type: "POST",
