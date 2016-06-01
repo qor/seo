9f732cb18d92d9aa0da5d9e61a6e6513d3d05cc1
binku <binku87@gmail.com>
2016-06-01 11:19:04 +0800

    Add test for keywords

--- seo_test.go
+++ seo_test.go
@@ -57,16 +57,16 @@ type MicroDataTestCase struct {
 func TestRender(t *testing.T) {
 	var testCases []RenderTestCase
 	testCases = append(testCases,
-		RenderTestCase{"Qor", seo.Setting{Title: "", Description: "", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title></title><meta name="description" content="">`},
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}}", Description: "{{SiteName}}", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title>Qor</title><meta name="description" content="Qor">`},
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}}", Description: "{{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor Clothing</title><meta name="description" content="/clothing">`},
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}} {{Name}}", Description: "{{URLTitle}} {{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor Clothing Clothing</title><meta name="description" content="/clothing /clothing">`},
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}} {{URLTitle}}", Description: "{{SiteName}} {{Name}} {{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title>Qor  </title><meta name="description" content="Qor  ">`},
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name1}}", Description: "{{URLTitle1}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor {{Name1}}</title><meta name="description" content="{{URLTitle1}}">`},
+		RenderTestCase{"Qor", seo.Setting{Title: "", Description: "", Keywords: "", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title></title><meta name="description" content=""><meta name="keywords" content=""/>`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}}", Description: "{{SiteName}}", Keywords: "{{SiteName}}", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title>Qor</title><meta name="description" content="Qor"><meta name="keywords" content="Qor"/>`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}}", Description: "{{URLTitle}}", Keywords: "{{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor Clothing</title><meta name="description" content="/clothing"><meta name="keywords" content="/clothing"/>`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}} {{Name}}", Description: "{{URLTitle}} {{URLTitle}}", Keywords: "{{URLTitle}} {{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor Clothing Clothing</title><meta name="description" content="/clothing /clothing"><meta name="keywords" content="/clothing /clothing"/>`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}} {{URLTitle}}", Description: "{{SiteName}} {{Name}} {{URLTitle}}", Keywords: "{{SiteName}} {{Name}} {{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "", "", false, `<title>Qor  </title><meta name="description" content="Qor  "><meta name="keywords" content="Qor  "/>`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name1}}", Description: "{{URLTitle1}}", Keywords: "{{URLTitle1}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", false, `<title>Qor {{Name1}}</title><meta name="description" content="{{URLTitle1}}"><meta name="keywords" content="{{URLTitle1}}"/>`},
 		// Pass nil object for Render
-		RenderTestCase{"Qor", seo.Setting{}, "Clothing", "/clothing", false, `<title></title><meta name="description" content="">`},
+		RenderTestCase{"Qor", seo.Setting{}, "Clothing", "/clothing", false, `<title></title><meta name="description" content=""><meta name="keywords" content=""/>`},
 		// Pass a non struct object
-		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}}", Description: "{{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", true, `<title>{{SiteName}} {{Name}}</title><meta name="description" content="{{URLTitle}}">`},
+		RenderTestCase{"Qor", seo.Setting{Title: "{{SiteName}} {{Name}}", Description: "{{URLTitle}}", Tags: "Name,URLTitle", TagsArray: nil}, "Clothing", "/clothing", true, `<title>{{SiteName}} {{Name}}</title><meta name="description" content="{{URLTitle}}"><meta name="keywords" content=""/>`},
 	)
 	seo := Seo{}
 	cat := Category{}
