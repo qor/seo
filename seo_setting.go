0d23451676f2e8544739b5d073ed86e29a8d6043
Azuma Fan <azuma@theplant.jp>
2020-01-16 13:42:32 +0800

    remove EnabledCustomize Valuer

--- seo_setting.go
+++ seo_setting.go
@@ -295,7 +295,7 @@ func (setting Setting) ConfigureQorResource(res resource.Resourcer) {
 		res.Meta(&admin.Meta{Name: "Description", Label: "Meta Description"})
 		res.Meta(&admin.Meta{Name: "Keywords", Label: "Meta Keywords"})
 		res.Meta(&admin.Meta{Name: "Type", Type: "hidden"})
-		res.Meta(&admin.Meta{Name: "EnabledCustomize", Type: "hidden", Valuer: func(interface{}, *qor.Context) interface{} { return "" }})
+		res.Meta(&admin.Meta{Name: "EnabledCustomize", Type: "hidden"})
 		res.Meta(&admin.Meta{Name: "OpenGraphImageFromMediaLibrary", Label: "Open Graph Image", Config: &media_library.MediaBoxConfig{
 			Max:       1,
 			AllowType: media_library.ALLOW_TYPE_IMAGE,
